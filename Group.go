4830f609b3e24b8751a5b34dc9a3b51a8a5f06f5
Eduard Urbach <e.urbach@gmail.com>
2019-11-03 18:39:19 +0900

    Minor change

--- Group.go
+++ Group.go
@@ -2,4 +2,5 @@ package main
 
 // Anything that uses the parentheses '(' and ')' is called a group.
 // Function calls and function definitions use groups to group up parameters.
+// Mathematical expressions use groups to control operator precedence.
 type Group interface{}
