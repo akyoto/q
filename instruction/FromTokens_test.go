f8e1fb92812280d2ecc884613a3a883e8a377f24
Eduard Urbach <e.urbach@gmail.com>
2019-11-09 15:21:27 +0900

    Improved test coverage

--- instruction/FromTokens_test.go
+++ instruction/FromTokens_test.go
@@ -9,15 +9,25 @@ import (
 )
 
 func TestFromTokens(t *testing.T) {
-	source := []byte("\na = 1\nb()\nreturn\n")
+	source := []byte("\na = 1\nb()\nreturn\na)\n")
+	expected := []instruction.Instruction{
+		{instruction.Assignment, nil, 1},
+		{instruction.Call, nil, 5},
+		{instruction.Keyword, nil, 9},
+		{instruction.Invalid, nil, 11},
+	}
+
 	tokens := []token.Token{}
 	processed := 0
 	tokens, processed = token.Tokenize(source, tokens)
 	assert.Equal(t, processed, len(source))
-	assert.Equal(t, len(tokens), 11)
+	assert.Equal(t, len(tokens), 14)
 	instructions := instruction.FromTokens(tokens)
-	assert.Equal(t, len(instructions), 3)
-	assert.Equal(t, instructions[0].Kind, instruction.Assignment)
-	assert.Equal(t, instructions[1].Kind, instruction.Call)
-	assert.Equal(t, instructions[2].Kind, instruction.Keyword)
+	assert.Equal(t, len(instructions), 4)
+
+	for index := range instructions {
+		assert.Equal(t, instructions[index].Kind, expected[index].Kind)
+		assert.Equal(t, instructions[index].Position, expected[index].Position)
+		assert.Equal(t, instructions[index].Kind.String(), expected[index].Kind.String())
+	}
 }
--- instruction/Instruction.go
+++ instruction/Instruction.go
@@ -5,7 +5,7 @@ import "github.com/akyoto/q/token"
 // Instruction encapsulates a single instruction inside a function.
 // Instructions can be variable assignments, function calls or keywords.
 type Instruction struct {
-	Tokens   []token.Token
 	Kind     Kind
+	Tokens   []token.Token
 	Position token.Position
 }
