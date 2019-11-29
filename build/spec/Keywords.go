835f594a068cbad3305d4805dac9dcd77713e702
Eduard Urbach <e.urbach@gmail.com>
2019-11-29 09:23:24 +0900

    Added a few keywords

--- build/spec/Keywords.go
+++ build/spec/Keywords.go
@@ -2,9 +2,11 @@ package spec
 
 // Keywords defines the keywords used in the language.
 var Keywords = map[string]bool{
-	"if":     true,
-	"for":    true,
-	"loop":   true,
-	"mut":    true,
-	"return": true,
+	"if":      true,
+	"ensure":  true,
+	"for":     true,
+	"loop":    true,
+	"mut":     true,
+	"require": true,
+	"return":  true,
 }
