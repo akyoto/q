6e8f3d13ff5aea7f95d5e4449905393aeed8396b
Eduard Urbach <e.urbach@gmail.com>
2019-12-02 14:25:01 +0900

    Fixed tests

--- build/Build_test.go
+++ build/Build_test.go
@@ -26,7 +26,7 @@ func TestExamples(t *testing.T) {
 		{"hello", "Hello\n", 0},
 		{"procedures", "Procedure 1\nProcedure 2\nProcedure 3\n", 0},
 		{"functions", "123456789\n123456789\n123456789\n", 0},
-		{"syscalls", "123456789\n", 0},
+		{"syscalls", "", 0},
 		{"loops", "Hello\nHello\nHello\n", 0},
 		{"fibonacci", "", 89},
 	}
