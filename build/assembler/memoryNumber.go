abeea6b240f6b9cb8f007627dfade7802f907496
Eduard Urbach <e.urbach@gmail.com>
2019-12-09 18:35:25 +0900

    Improved assembly viewer

--- build/assembler/memoryNumber.go
+++ build/assembler/memoryNumber.go
@@ -50,5 +50,5 @@ func (instr *memoryNumber) Size() byte {
 
 // String implements the string serialization.
 func (instr *memoryNumber) String() string {
-	return fmt.Sprintf("[%d]   %s [%v], %d", instr.size, mnemonicColor.Sprint(instr.Mnemonic), instr.Destination.StringWithUser(instr.UsedBy), instr.Number)
+	return fmt.Sprintf("[%d]   %s %db [%v+%d], %d", instr.size, mnemonicColor.Sprint(instr.Mnemonic), instr.ByteCount, instr.Destination.StringWithUser(instr.UsedBy), instr.Offset, instr.Number)
 }
