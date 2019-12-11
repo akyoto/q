babda18cb023570b2edc92e573ddc50aa82fdf09
Eduard Urbach <e.urbach@gmail.com>
2019-12-11 14:27:13 +0900

    Improved file processing

--- build/File.go
+++ build/File.go
@@ -2,7 +2,8 @@ package build
 
 import (
 	"fmt"
-	"io/ioutil"
+	"io"
+	"os"
 	"sync/atomic"
 
 	"github.com/akyoto/q/build/errors"
@@ -33,33 +34,52 @@ func NewFile(inputFile string) *File {
 // Tokenize converts the entire file contents to a list of tokens.
 func (file *File) Tokenize() error {
 	var (
-		err       error
-		processed uint16
+		fd            *os.File
+		err           error
+		processed     uint16
+		contents      []byte
+		bufferOnStack [4096]byte
+		buffer        = bufferOnStack[:]
 	)
 
-	file.contents, err = ioutil.ReadFile(file.path)
+	fd, err = os.Open(file.path)
 
 	if err != nil {
 		return err
 	}
 
+	defer fd.Close()
+
+	for {
+		n, err := fd.Read(buffer)
+		contents = append(contents, buffer[:n]...)
+
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+
+			return err
+		}
+	}
+
 	// Dividing the file length by 2 is a good approximation
 	// of the number of tokens in the file.
 	// For very small files we add a minimum of 4 reserved tokens.
-	guessTokenCount := len(file.contents)/2 + 4
+	guessTokenCount := len(contents)/2 + 4
 	tokens := make([]token.Token, 0, guessTokenCount)
 
 	// Process tokens
-	file.tokens, processed = token.Tokenize(file.contents, tokens)
+	tokens, processed = token.Tokenize(contents, tokens)
 
 	// Warn about missing final newline.
-	if file.contents[len(file.contents)-1] != '\n' {
-		return NewError(errors.MissingEndingNewline, file.path, file.tokens, nil)
+	if contents[len(contents)-1] != '\n' {
+		return NewError(errors.MissingEndingNewline, file.path, tokens, nil)
 	}
 
 	// If we didn't process everything, there's some error in the tokenization.
-	if processed != uint16(len(file.contents)) {
-		remaining := file.contents[processed:]
+	if processed != uint16(len(contents)) {
+		remaining := contents[processed:]
 		until := len(remaining)
 
 		for i, c := range remaining {
@@ -73,9 +93,11 @@ func (file *File) Tokenize() error {
 			Expression: string(remaining[:until]),
 		}
 
-		return NewError(err, file.path, file.tokens, nil)
+		return NewError(err, file.path, tokens, nil)
 	}
 
+	file.contents = contents
+	file.tokens = tokens
 	return nil
 }
 
