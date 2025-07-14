4a9d95a9939c6770bcd4e3c69bddef50aab33cc4
Eduard Urbach <eduard@urbach.dev>
2025-07-14 11:41:16 +0200

    Moved error tests to core

---
+++ src/core/errors_test.go
@@ -0,0 +1,41 @@
+package core_test
+
+import (
+	"path/filepath"
+	"strings"
+	"testing"
+
+	"git.urbach.dev/cli/q/src/compiler"
+	"git.urbach.dev/cli/q/src/config"
+	"git.urbach.dev/cli/q/src/core"
+	"git.urbach.dev/go/assert"
+)
+
+var errs = []struct {
+	File          string
+	ExpectedError error
+}{
+	{"ParameterCountMismatch.q", &core.ParameterCountMismatch{Function: "main.f", Count: 0, ExpectedCount: 1}},
+	{"ParameterCountMismatch2.q", &core.ParameterCountMismatch{Function: "main.f", Count: 2, ExpectedCount: 1}},
+	{"TypeMismatch.q", &core.TypeMismatch{Encountered: "string", Expected: "int64", ParameterName: "x", IsReturn: false}},
+	{"UnknownIdentifier.q", &core.UnknownIdentifier{Name: "x"}},
+	{"UnknownIdentifier2.q", &core.UnknownIdentifier{Name: "x"}},
+	{"UnknownIdentifier3.q", &core.UnknownIdentifier{Name: "x"}},
+	{"UnknownIdentifier4.q", &core.UnknownIdentifier{Name: "unknown"}},
+	{"UnknownIdentifier5.q", &core.UnknownIdentifier{Name: "unknown"}},
+	{"UnknownIdentifier6.q", &core.UnknownIdentifier{Name: "os"}},
+	{"UnknownIdentifier7.q", &core.UnknownIdentifier{Name: "os.unknown"}},
+}
+
+func TestErrors(t *testing.T) {
+	for _, test := range errs {
+		name := strings.TrimSuffix(test.File, ".q")
+
+		t.Run(name, func(t *testing.T) {
+			b := config.New(filepath.Join("testdata", test.File))
+			_, err := compiler.Compile(b)
+			assert.NotNil(t, err)
+			assert.Contains(t, err.Error(), test.ExpectedError.Error())
+		})
+	}
+}
