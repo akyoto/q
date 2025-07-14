1e9c3f507425fbe950242c7f79853c7ec4cc1534
Eduard Urbach <eduard@urbach.dev>
2025-07-14 14:40:53 +0200

    Added more asm tests

--- src/asm/Assembler_test.go
+++ src/asm/Assembler_test.go
@@ -5,6 +5,7 @@ import (
 
 	"git.urbach.dev/cli/q/src/asm"
 	"git.urbach.dev/cli/q/src/config"
+	"git.urbach.dev/cli/q/src/cpu"
 	"git.urbach.dev/go/assert"
 )
 
@@ -12,30 +13,81 @@ func TestAssembler(t *testing.T) {
 	a := &asm.Assembler{}
 	a.Append(&asm.Label{Name: "a"})
 	a.Append(&asm.StackFrameStart{})
+	a.Append(&asm.Jump{Label: "b"})
+	a.Append(&asm.Jump{Label: "c"})
+	a.Append(&asm.Jump{Label: "d"})
+	a.Append(&asm.Jump{Label: "e"})
+	a.Append(&asm.Jump{Label: "f"})
 	a.Append(&asm.Call{Label: "b"})
 	a.Append(&asm.Call{Label: "c"})
-	a.Append(&asm.MoveRegisterLabel{Label: "b"})
-	a.Append(&asm.MoveRegisterNumber{Destination: 0, Number: 123})
-	a.Append(&asm.MoveRegisterRegister{Destination: 0, Source: 1})
+	a.Append(&asm.Call{Label: "d"})
+	a.Append(&asm.Call{Label: "e"})
+	a.Append(&asm.Call{Label: "f"})
 	a.Append(&asm.StackFrameEnd{})
 	a.Append(&asm.Return{})
 
 	b := &asm.Assembler{}
 	b.Append(&asm.Label{Name: "b"})
-	b.Append(&asm.Syscall{})
+	b.Append(&asm.AddRegisterRegister{Destination: 0, Source: 1, Operand: 2})
+	b.Append(&asm.SubRegisterRegister{Destination: 0, Source: 1, Operand: 2})
+	b.Append(&asm.MulRegisterRegister{Destination: 0, Source: 1, Operand: 2})
+	b.Append(&asm.DivRegisterRegister{Destination: 0, Source: 1, Operand: 2})
+	b.Append(&asm.AddRegisterRegister{Destination: 1, Source: 1, Operand: 2})
+	b.Append(&asm.SubRegisterRegister{Destination: 1, Source: 1, Operand: 2})
+	b.Append(&asm.MulRegisterRegister{Destination: 1, Source: 1, Operand: 2})
+	b.Append(&asm.DivRegisterRegister{Destination: 1, Source: 1, Operand: 2})
 	b.Append(&asm.Return{})
 
 	c := &asm.Assembler{}
 	c.Append(&asm.Label{Name: "c"})
-	c.Append(&asm.Jump{Label: "branch2"})
-	c.Append(&asm.Label{Name: "branch1"})
-	c.Append(&asm.Label{Name: "branch2"})
+	c.Append(&asm.PushRegisters{Registers: []cpu.Register{0}})
+	c.Append(&asm.PushRegisters{Registers: []cpu.Register{0, 1}})
+	c.Append(&asm.PushRegisters{Registers: []cpu.Register{0, 1, 2}})
+	c.Append(&asm.PushRegisters{Registers: []cpu.Register{0, 1, 2, 3}})
+	c.Append(&asm.PushRegisters{Registers: []cpu.Register{0, 1, 2, 3, 4}})
+	c.Append(&asm.PopRegisters{Registers: []cpu.Register{0, 1, 2, 3, 4}})
+	c.Append(&asm.PopRegisters{Registers: []cpu.Register{0, 1, 2, 3}})
+	c.Append(&asm.PopRegisters{Registers: []cpu.Register{0, 1, 2}})
+	c.Append(&asm.PopRegisters{Registers: []cpu.Register{0, 1}})
+	c.Append(&asm.PopRegisters{Registers: []cpu.Register{0}})
 	c.Append(&asm.Return{})
+
+	d := &asm.Assembler{}
+	d.Append(&asm.Label{Name: "d"})
+	d.Append(&asm.MoveRegisterLabel{Destination: 0, Label: "a"})
+	d.Append(&asm.MoveRegisterLabel{Destination: 0, Label: "b"})
+	d.Append(&asm.MoveRegisterLabel{Destination: 0, Label: "c"})
+	d.Append(&asm.MoveRegisterLabel{Destination: 0, Label: "d"})
+	d.Append(&asm.MoveRegisterLabel{Destination: 0, Label: "e"})
+	d.Append(&asm.MoveRegisterNumber{Destination: 0, Number: 123})
+	d.Append(&asm.MoveRegisterRegister{Destination: 0, Source: 1})
+	d.Append(&asm.Return{})
+
+	e := &asm.Assembler{}
+	e.Append(&asm.Label{Name: "e"})
+	a.Append(&asm.StackFrameStart{})
+	e.Append(&asm.Jump{Label: "branch2"})
+	e.Append(&asm.Label{Name: "branch1"})
+	e.Append(&asm.Label{Name: "branch2"})
+	e.Append(&asm.Syscall{})
+	e.Append(&asm.StackFrameEnd{})
+	e.Append(&asm.Return{})
+
+	f := &asm.Assembler{}
+	f.Libraries.Append("kernel32", "ExitProcess")
+	f.Append(&asm.Label{Name: "f"})
+	f.Append(&asm.CallExternStart{})
+	f.Append(&asm.CallExtern{Library: "kernel32", Function: "ExitProcess"})
+	f.Append(&asm.CallExternEnd{})
+	f.Append(&asm.Return{})
 
 	final := asm.Assembler{}
 	final.Merge(a)
 	final.Merge(b)
 	final.Merge(c)
+	final.Merge(d)
+	final.Merge(e)
+	final.Merge(f)
 
 	code, _, _ := final.Compile(&config.Build{Arch: config.ARM})
 	assert.NotNil(t, code)
