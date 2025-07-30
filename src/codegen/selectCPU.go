e3c16ee06820678a199693466f15e74a1e67daa0
Eduard Urbach <eduard@urbach.dev>
2025-07-30 20:14:35 +0200

    Added utility function for CPU selection

---
+++ src/codegen/selectCPU.go
@@ -0,0 +1,33 @@
+package codegen
+
+import (
+	"git.urbach.dev/cli/q/src/arm"
+	"git.urbach.dev/cli/q/src/config"
+	"git.urbach.dev/cli/q/src/cpu"
+	"git.urbach.dev/cli/q/src/x86"
+)
+
+func selectCPU(build *config.Build) *cpu.CPU {
+	switch build.Arch {
+	case config.ARM:
+		switch build.OS {
+		case config.Linux:
+			return &arm.LinuxCPU
+		case config.Mac:
+			return &arm.MacCPU
+		case config.Windows:
+			return &arm.WindowsCPU
+		}
+	case config.X86:
+		switch build.OS {
+		case config.Linux:
+			return &x86.LinuxCPU
+		case config.Mac:
+			return &x86.MacCPU
+		case config.Windows:
+			return &x86.WindowsCPU
+		}
+	}
+
+	return nil
+}
