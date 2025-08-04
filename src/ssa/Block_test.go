5ee50076738fe513d423a227cf7b0a1c521f3529
Eduard Urbach <eduard@urbach.dev>
2025-08-04 02:04:36 +0200

    Added more tests for block connections

--- src/ssa/Block_test.go
+++ src/ssa/Block_test.go
@@ -7,15 +7,18 @@ import (
 	"git.urbach.dev/go/assert"
 )
 
+func TestBlockCanReachSelf(t *testing.T) {
+	a := ssa.NewBlock("a")
+	assert.True(t, a.CanReachPredecessor(a))
+}
+
 func TestBlockCanReachPredecessor(t *testing.T) {
 	a := ssa.NewBlock("a")
 	b := ssa.NewBlock("b")
 	a.AddSuccessor(b)
 
-	assert.True(t, a.CanReachPredecessor(a))
-	assert.True(t, b.CanReachPredecessor(a))
-	assert.True(t, b.CanReachPredecessor(b))
 	assert.False(t, a.CanReachPredecessor(b))
+	assert.True(t, b.CanReachPredecessor(a))
 }
 
 func TestBlockCanReachPredecessorBranch(t *testing.T) {
@@ -28,16 +31,21 @@ func TestBlockCanReachPredecessorBranch(t *testing.T) {
 	b.AddSuccessor(d)
 	c.AddSuccessor(d)
 
-	assert.True(t, a.CanReachPredecessor(a))
+	assert.False(t, a.CanReachPredecessor(b))
+	assert.False(t, a.CanReachPredecessor(c))
+	assert.False(t, a.CanReachPredecessor(d))
+
 	assert.True(t, b.CanReachPredecessor(a))
+	assert.False(t, b.CanReachPredecessor(c))
+	assert.False(t, b.CanReachPredecessor(d))
+
 	assert.True(t, c.CanReachPredecessor(a))
+	assert.False(t, c.CanReachPredecessor(b))
+	assert.False(t, c.CanReachPredecessor(d))
+
 	assert.True(t, d.CanReachPredecessor(a))
 	assert.True(t, d.CanReachPredecessor(b))
 	assert.True(t, d.CanReachPredecessor(c))
-	assert.False(t, b.CanReachPredecessor(c))
-	assert.False(t, c.CanReachPredecessor(b))
-	assert.False(t, b.CanReachPredecessor(d))
-	assert.False(t, c.CanReachPredecessor(d))
 }
 
 func TestBlockCanReachPredecessorLoop(t *testing.T) {
@@ -48,13 +56,39 @@ func TestBlockCanReachPredecessorLoop(t *testing.T) {
 	b.AddSuccessor(b)
 	b.AddSuccessor(c)
 
-	assert.True(t, a.CanReachPredecessor(a))
 	assert.False(t, a.CanReachPredecessor(b))
 	assert.False(t, a.CanReachPredecessor(c))
+
 	assert.True(t, b.CanReachPredecessor(a))
-	assert.True(t, b.CanReachPredecessor(b))
 	assert.False(t, b.CanReachPredecessor(c))
+
 	assert.True(t, c.CanReachPredecessor(a))
 	assert.True(t, c.CanReachPredecessor(b))
-	assert.True(t, c.CanReachPredecessor(c))
+}
+
+func TestBlockCanReachPredecessorLoopExtended(t *testing.T) {
+	a := ssa.NewBlock("a")
+	b := ssa.NewBlock("b")
+	c := ssa.NewBlock("c")
+	d := ssa.NewBlock("d")
+	a.AddSuccessor(b)
+	b.AddSuccessor(c)
+	b.AddSuccessor(d)
+	c.AddSuccessor(b)
+
+	assert.False(t, a.CanReachPredecessor(b))
+	assert.False(t, a.CanReachPredecessor(c))
+	assert.False(t, a.CanReachPredecessor(d))
+
+	assert.True(t, b.CanReachPredecessor(a))
+	assert.True(t, b.CanReachPredecessor(c))
+	assert.False(t, b.CanReachPredecessor(d))
+
+	assert.True(t, c.CanReachPredecessor(a))
+	assert.True(t, c.CanReachPredecessor(b))
+	assert.False(t, c.CanReachPredecessor(d))
+
+	assert.True(t, d.CanReachPredecessor(a))
+	assert.True(t, d.CanReachPredecessor(b))
+	assert.True(t, d.CanReachPredecessor(c))
 }
