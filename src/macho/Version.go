5e41011a5708543ec4831ea885cfbcfdb8e9e576
Eduard Urbach <eduard@urbach.dev>
2025-08-08 11:55:40 +0200

    Fixed build version encoding for Mac binaries

--- src/macho/Version.go
+++ src/macho/Version.go
@@ -2,5 +2,5 @@ package macho
 
 // Version creates the version for the build version load command.
 func Version(major uint32, minor uint32, patch uint32) uint32 {
-	return major<<24 | minor<<16 | patch<<8
+	return major<<16 | minor<<8 | patch
 }
