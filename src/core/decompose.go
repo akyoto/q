4caf7e0cc965c3aa060d25ee6f162e8bd5f908f1
Eduard Urbach <eduard@urbach.dev>
2025-08-20 21:35:13 +0200

    Implemented extern call by value for 16-byte structs

--- src/core/decompose.go
+++ src/core/decompose.go
@@ -31,21 +31,34 @@ func (f *Function) decompose(nodes []*expression.Expression, typeCheck []*ssa.Pa
 		structure, isStruct := value.(*ssa.Struct)
 
 		if isStruct {
-			if structure.Typ.Size() <= 8 {
+			switch {
+			case structure.Typ.Size() <= 16:
 				// Packed integer: Use the first argument,
 				// then bitwise OR with the shifted field values.
 				cursor := structure.Arguments[0]
 				size := structure.Typ.Fields[0].Type.Size()
 
 				for i, field := range structure.Arguments[1:] {
+					fieldSize := structure.Typ.Fields[i+1].Type.Size()
+
+					if size+fieldSize > 8 {
+						// The field doesn't fit into the register anymore.
+						// We need to use this field as the starting value
+						// for the next argument.
+						args = append(args, cursor)
+						cursor = field
+						size = fieldSize
+						continue
+					}
+
 					sizeValue := f.Append(&ssa.Int{Int: size * 8})
 					shifted := f.Append(&ssa.BinaryOp{Op: token.Shl, Left: field, Right: sizeValue})
 					cursor = f.Append(&ssa.BinaryOp{Op: token.Or, Left: cursor, Right: shifted})
-					size += structure.Typ.Fields[i+1].Type.Size()
+					size += fieldSize
 				}
 
 				args = append(args, cursor)
-			} else {
+			default:
 				for _, field := range structure.Arguments {
 					args = append(args, field)
 				}
