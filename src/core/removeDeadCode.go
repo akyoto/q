0893e9585b58a95d018b17173431613e1690d24a
Eduard Urbach <eduard@urbach.dev>
2025-09-18 22:21:16 +0200

    Refactored dead code removal

--- src/core/removeDeadCode.go
+++ src/core/removeDeadCode.go
@@ -1,7 +1,6 @@
 package core
 
 import (
-	"git.urbach.dev/cli/q/src/errors"
 	"git.urbach.dev/cli/q/src/ssa"
 )
 
@@ -9,52 +8,11 @@ import (
 func (f *Function) removeDeadCode(folded map[ssa.Value]struct{}) error {
 	for _, block := range f.Blocks {
 		for i, value := range block.Instructions {
-			if !value.IsPure() {
-				continue
+			err := f.removeDeadValue(block, i, value, folded)
+
+			if err != nil {
+				return err
 			}
-
-			if len(value.Users()) > 0 {
-				continue
-			}
-
-			_, isFolded := folded[value]
-
-			if isFolded {
-				block.RemoveAt(i)
-				continue
-			}
-
-			phi, isPhi := value.(*ssa.Phi)
-
-			if isPhi {
-				if phi.IsPartiallyUndefined() {
-					block.RemoveAt(i)
-					continue
-				}
-
-				value = phi.FirstDefined()
-			}
-
-			structField, isFieldOfStruct := value.(ssa.StructField)
-
-			if isFieldOfStruct && structField.Struct() != nil {
-				partiallyUnused := false
-
-				for _, field := range structField.Struct().Arguments {
-					if len(field.Users()) > 0 {
-						partiallyUnused = true
-						break
-					}
-				}
-
-				if partiallyUnused {
-					block.RemoveAt(i)
-					continue
-				}
-			}
-
-			source := value.(ssa.HasSource)
-			return errors.New(&UnusedValue{Value: source.StringFrom(f.File.Bytes)}, f.File, source.Start())
 		}
 
 		block.RemoveNilValues()
---
+++ src/core/removeDeadValue.go
@@ -0,0 +1,56 @@
+package core
+
+import (
+	"git.urbach.dev/cli/q/src/errors"
+	"git.urbach.dev/cli/q/src/ssa"
+)
+
+// removeDeadValue checks if the value is dead and removes it.
+func (f *Function) removeDeadValue(block *ssa.Block, i int, value ssa.Value, folded map[ssa.Value]struct{}) error {
+	if !value.IsPure() {
+		return nil
+	}
+
+	if len(value.Users()) > 0 {
+		return nil
+	}
+
+	_, isFolded := folded[value]
+
+	if isFolded {
+		block.RemoveAt(i)
+		return nil
+	}
+
+	phi, isPhi := value.(*ssa.Phi)
+
+	if isPhi {
+		if phi.IsPartiallyUndefined() {
+			block.RemoveAt(i)
+			return nil
+		}
+
+		value = phi.FirstDefined()
+	}
+
+	structField, isFieldOfStruct := value.(ssa.StructField)
+
+	if isFieldOfStruct && structField.Struct() != nil {
+		partiallyUnused := false
+
+		for _, field := range structField.Struct().Arguments {
+			if len(field.Users()) > 0 {
+				partiallyUnused = true
+				break
+			}
+		}
+
+		if partiallyUnused {
+			block.RemoveAt(i)
+			return nil
+		}
+	}
+
+	source := value.(ssa.HasSource)
+	return errors.New(&UnusedValue{Value: source.StringFrom(f.File.Bytes)}, f.File, source.Start())
+}
