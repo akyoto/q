ec3bdbc07982327b59b0ccea9d18bc0961d67c3f
Eduard Urbach <eduard@urbach.dev>
2025-09-30 20:05:16 +0200

    Reduced amount of padding for mutable data

--- src/data/Finalize.go
+++ src/data/Finalize.go
@@ -1,7 +1,5 @@
 package data
 
-import "git.urbach.dev/cli/q/src/exe"
-
 // Finalize returns the final raw data slice and a map of labels with their respective indices.
 // It will try to reuse existing data whenever possible.
 func (data *Data) Finalize() ([]byte, map[string]int) {
@@ -18,16 +16,6 @@ func (data *Data) Finalize() ([]byte, map[string]int) {
 	final := make([]byte, 0, capacity)
 	positions := make(map[string]int, len(data.Immutable)+len(data.Mutable))
 	final = data.appendImmutable(final, positions)
-
-	if len(data.Mutable) > 0 {
-		_, padding := exe.AlignPad(len(final), 16)
-
-		for range padding {
-			final = append(final, 0)
-		}
-
-		final = data.appendMutable(final, positions)
-	}
-
+	final = data.appendMutable(final, positions)
 	return final, positions
 }
