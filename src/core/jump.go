fc187406db71349d7ef79269356a7dc3b1199461
Eduard Urbach <eduard@urbach.dev>
2025-10-12 20:12:37 +0200

    Fixed multiple jumps appearing in a single block

--- src/core/jump.go
+++ src/core/jump.go
@@ -2,13 +2,11 @@ package core
 
 import "git.urbach.dev/cli/q/src/ssa"
 
-// jump inserts a `jump` instruction to the block and adds it as a successor.
-// It performs a check to see if the last instruction was a `return` instruction.
-// In case `return` was found, it will not do anything.
+// jump inserts a `jump` instruction to the block and adds it as a successor
+// if the last instruction was not a `jump` or `return`.
 func (f *Function) jump(block *ssa.Block) {
-	_, returned := f.Block().Last().(*ssa.Return)
-
-	if returned {
+	switch f.Block().Last().(type) {
+	case *ssa.Jump, *ssa.Return:
 		return
 	}
 
