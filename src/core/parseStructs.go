e544201b84dd351fe5ae6bdc8eddd4566bc7e3c1
Eduard Urbach <eduard@urbach.dev>
2026-05-25 00:20:35 +0200

    Fixed incorrect processing of string type in structs

--- src/core/parseStructs.go
+++ src/core/parseStructs.go
@@ -8,7 +8,7 @@ import (
 
 // parseStructs parses the tokens of the struct field types.
 func (env *Environment) parseStructs(structs iter.Seq[*types.Struct]) error {
-	processed := map[*types.Struct]recursionState{}
+	processed := map[*types.Struct]recursionState{types.String: finished}
 
 	for structure := range structs {
 		err := env.parseStruct(structure, processed)
