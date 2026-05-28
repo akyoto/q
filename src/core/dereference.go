9c6a3873f6c980cb396664b55f75f408c39bc5be
Eduard Urbach <eduard@urbach.dev>
2026-05-28 13:24:44 +0200

    Refactored dereferencing of values

---
+++ src/core/dereference.go
@@ -0,0 +1,43 @@
+package core
+
+import (
+	"git.urbach.dev/cli/q/src/ssa"
+	"git.urbach.dev/cli/q/src/types"
+)
+
+// dereference loads from memory if the value was a memory address, otherwise returns the raw value.
+func (f *Function) dereference(value ssa.Value) ssa.Value {
+	switch value := value.(type) {
+	case *ssa.Data:
+		zero := f.Append(&ssa.Int{Int: 0})
+
+		load := f.Append(&ssa.Load{
+			Memory: &ssa.Memory{
+				Address: value,
+				Index:   zero,
+				Typ:     value.Typ.(*types.Pointer).To,
+			},
+			Source: value.Source,
+		})
+
+		return load
+
+	case *ssa.Memory:
+		switch typ := value.Typ.(type) {
+		case *types.Struct:
+			structure := f.loadFields(value, typ, value.Source)
+			return structure
+
+		default:
+			load := f.Append(&ssa.Load{
+				Memory: value,
+				Source: value.Source,
+			})
+
+			return load
+		}
+
+	default:
+		return value
+	}
+}
--- src/core/evaluateRight.go
+++ src/core/evaluateRight.go
@@ -3,11 +3,9 @@ package core
 import (
 	"git.urbach.dev/cli/q/src/expression"
 	"git.urbach.dev/cli/q/src/ssa"
-	"git.urbach.dev/cli/q/src/types"
 )
 
-// evaluateRight creates a load from memory if the value
-// was a memory address, otherwise returns the raw value.
+// evaluateRight evaluates the expression and automatically dereferences it if needed.
 func (f *Function) evaluateRight(expr *expression.Expression) (ssa.Value, error) {
 	value, err := f.evaluate(expr)
 
@@ -15,40 +13,5 @@ func (f *Function) evaluateRight(expr *expression.Expression) (ssa.Value, error)
 		return nil, err
 	}
 
-	data, isData := value.(*ssa.Data)
-
-	if isData {
-		zero := f.Append(&ssa.Int{Int: 0})
-
-		load := f.Append(&ssa.Load{
-			Memory: &ssa.Memory{
-				Address: data,
-				Index:   zero,
-				Typ:     data.Typ.(*types.Pointer).To,
-			},
-			Source: data.Source,
-		})
-
-		return load, nil
-	}
-
-	memory, isMemory := value.(*ssa.Memory)
-
-	if !isMemory {
-		return value, nil
-	}
-
-	switch typ := memory.Typ.(type) {
-	case *types.Struct:
-		structure := f.loadFields(memory, typ, expr.Source())
-		return structure, nil
-
-	default:
-		load := f.Append(&ssa.Load{
-			Memory: memory,
-			Source: memory.Source,
-		})
-
-		return load, nil
-	}
+	return f.dereference(value), nil
 }
