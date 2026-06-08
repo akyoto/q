24c636a48e189841aaef75847d9f94ea1225c7da
Eduard Urbach <eduard@urbach.dev>
2026-06-08 20:28:16 +0200

    Improved verbose test coverage

--- src/verbose/Function_test.go
+++ src/verbose/Function_test.go
@@ -1,18 +1,26 @@
 package verbose_test
 
 import (
+	"path/filepath"
 	"testing"
 
 	"git.urbach.dev/cli/q/src/compiler"
 	"git.urbach.dev/cli/q/src/config"
+	"git.urbach.dev/cli/q/src/fs"
 	"git.urbach.dev/cli/q/src/verbose"
 	"git.urbach.dev/go/assert"
 )
 
 func TestVerboseOutput(t *testing.T) {
-	b := config.New("../../examples/hello")
-	env, err := compiler.Compile(b)
-	assert.Nil(t, err)
-	verbose.ASM(env.Init)
-	verbose.SSA(env.Init)
+	examples := "../../examples"
+
+	fs.Walk(examples, func(dir string) {
+		t.Run(dir, func(t *testing.T) {
+			b := config.New(filepath.Join(examples, dir))
+			env, err := compiler.Compile(b)
+			assert.Nil(t, err)
+			verbose.ASM(env.Init)
+			verbose.SSA(env.Init)
+		})
+	})
 }
