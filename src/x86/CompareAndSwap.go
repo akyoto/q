4b96d11fa8742bbbab2812663686598f5f674b01
Eduard Urbach <eduard@urbach.dev>
2026-06-26 17:22:31 +0200

    Implemented x86-64 encoding for the cmpxchg instruction

---
+++ src/x86/CompareAndSwap.go
@@ -0,0 +1,10 @@
+package x86
+
+import "git.urbach.dev/cli/q/src/cpu"
+
+// CompareAndSwap compares R0 with the memory contents.
+// If they are equal, source register is written to memory.
+// If they are not equal, R0 is loaded with the value in memory.
+func CompareAndSwap(code []byte, base cpu.Register, offset cpu.Register, scale Scale, length byte, source cpu.Register) []byte {
+	return memAccessDynamic(code, source, base, offset, scale, length, length, opCodePrefix0F, 0xB0, 0xB1)
+}
