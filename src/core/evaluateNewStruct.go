868eb2b1198dc524823bd373528799efefe19c9d
Eduard Urbach <eduard@urbach.dev>
2026-07-02 17:40:55 +0200

    Fixed panic when attempting struct initialization on a slice

--- src/core/evaluateNewStruct.go
+++ src/core/evaluateNewStruct.go
@@ -9,37 +9,45 @@ import (
 
 // evaluateNewStruct allocates a new struct with a field initializer list.
 func (f *Function) evaluateNewStruct(expr *expression.Expression) (ssa.Value, error) {
-	pointer, err := f.evaluateNew(expr.Children[0])
+	object, err := f.evaluateNew(expr.Children[0])
 
 	if err != nil {
 		return nil, err
 	}
 
-	typ := types.Unwrap(pointer.Type()).(*types.Pointer).To
-	structType, isStructType := typ.(*types.Struct)
+	switch typ := types.Unwrap(object.Type()).(type) {
+	case *types.Pointer:
+		structType, isStructType := typ.To.(*types.Struct)
 
-	if !isStructType {
-		return nil, errors.New(&NotDataStruct{TypeName: typ.Name()}, f.File, expr.Children[0].Children[1].Source())
+		if !isStructType {
+			return nil, errors.New(&NotDataStruct{TypeName: typ.To.Name()}, f.File, expr.Children[0].Children[1].Source())
+		}
+
+		for _, definition := range expr.Children[1:] {
+			if isTrailing(definition, expr.Children) {
+				continue
+			}
+
+			field, rightValue, err := f.extractField(structType, definition)
+
+			if err != nil {
+				return nil, err
+			}
+
+			memory := f.structField(object, field)
+			err = f.store(memory, rightValue)
+
+			if err != nil {
+				return nil, err
+			}
+		}
+
+	case *types.Struct:
+		return nil, errors.New(&NotImplemented{Subject: "struct initialization for arrays"}, f.File, expr.Source())
+
+	default:
+		panic("invalid struct initialization type")
 	}
 
-	for _, definition := range expr.Children[1:] {
-		if isTrailing(definition, expr.Children) {
-			continue
-		}
-
-		field, rightValue, err := f.extractField(structType, definition)
-
-		if err != nil {
-			return nil, err
-		}
-
-		memory := f.structField(pointer, field)
-		err = f.store(memory, rightValue)
-
-		if err != nil {
-			return nil, err
-		}
-	}
-
-	return pointer, nil
+	return object, nil
 }
