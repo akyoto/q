b10d50577aa35ab6b9bbd31cdf468d5a1828a6e1
Eduard Urbach <eduard@urbach.dev>
2026-07-13 08:56:35 +0200

    Added resolver tests

---
+++ src/resolver/resolver_test.go
@@ -0,0 +1,35 @@
+package resolver_test
+
+import (
+	"testing"
+
+	"git.urbach.dev/cli/q/src/config"
+	"git.urbach.dev/cli/q/src/core"
+	"git.urbach.dev/cli/q/src/fs"
+	"git.urbach.dev/cli/q/src/resolver"
+	"git.urbach.dev/cli/q/src/token"
+	"git.urbach.dev/cli/q/src/types"
+	"git.urbach.dev/go/assert"
+)
+
+func TestResolver(t *testing.T) {
+	build := config.New()
+	env := core.NewEnvironment(build)
+	src := []byte("global { x int }")
+
+	file := &fs.File{
+		Package: "main",
+		Bytes:   src,
+		Tokens:  token.Tokenize(src),
+	}
+
+	env.ReceiveGlobal(&core.Global{
+		Name:   "x",
+		File:   file,
+		Tokens: file.Tokens[2:4],
+	})
+
+	err := resolver.ResolveTypes(env)
+	assert.Nil(t, err)
+	assert.Equal(t, env.Packages["main"].Globals["x"].Typ.Name(), types.Int.Name())
+}
