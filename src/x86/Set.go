df43ce6916b72d7b2567c32e737d64ec8b5384b3
Eduard Urbach <eduard@urbach.dev>
2026-07-15 17:22:14 +0200

    Fixed incorrect conditional set encoding on x86-64

--- src/x86/Set.go
+++ src/x86/Set.go
@@ -4,50 +4,50 @@ import "git.urbach.dev/cli/q/src/cpu"
 
 // SetIfEqual sets the register to 1 if the result was equal, otherwise to 0.
 func SetIfEqual(code []byte, destination cpu.Register) []byte {
-	return append(code, 0x0F, 0x94, ModRM(AddressDirect, 0, byte(destination)))
+	return encode(code, AddressDirect, 0, destination, 1, 0x0F94)
 }
 
 // SetIfNotEqual sets the register to 1 if the result was not equal, otherwise to 0.
 func SetIfNotEqual(code []byte, destination cpu.Register) []byte {
-	return append(code, 0x0F, 0x95, ModRM(AddressDirect, 0, byte(destination)))
+	return encode(code, AddressDirect, 0, destination, 1, 0x0F95)
 }
 
 // SetIfGreater sets the register to 1 if the result was greater, otherwise to 0.
 func SetIfGreater(code []byte, destination cpu.Register) []byte {
-	return append(code, 0x0F, 0x9F, ModRM(AddressDirect, 0, byte(destination)))
+	return encode(code, AddressDirect, 0, destination, 1, 0x0F9F)
 }
 
 // SetIfGreaterEqual sets the register to 1 if the result was greater or equal, otherwise to 0.
 func SetIfGreaterEqual(code []byte, destination cpu.Register) []byte {
-	return append(code, 0x0F, 0x9D, ModRM(AddressDirect, 0, byte(destination)))
+	return encode(code, AddressDirect, 0, destination, 1, 0x0F9D)
 }
 
 // SetIfLess sets the register to 1 if the result was less, otherwise to 0.
 func SetIfLess(code []byte, destination cpu.Register) []byte {
-	return append(code, 0x0F, 0x9C, ModRM(AddressDirect, 0, byte(destination)))
+	return encode(code, AddressDirect, 0, destination, 1, 0x0F9C)
 }
 
 // SetIfLessEqual sets the register to 1 if the result was less or equal, otherwise to 0.
 func SetIfLessEqual(code []byte, destination cpu.Register) []byte {
-	return append(code, 0x0F, 0x9E, ModRM(AddressDirect, 0, byte(destination)))
+	return encode(code, AddressDirect, 0, destination, 1, 0x0F9E)
 }
 
 // SetIfUnsignedGreater sets the register to 1 if the result was greater using unsigned comparison, otherwise to 0.
 func SetIfUnsignedGreater(code []byte, destination cpu.Register) []byte {
-	return append(code, 0x0F, 0x97, ModRM(AddressDirect, 0, byte(destination)))
+	return encode(code, AddressDirect, 0, destination, 1, 0x0F97)
 }
 
 // SetIfUnsignedGreaterEqual sets the register to 1 if the result was greater or equal using unsigned comparison, otherwise to 0.
 func SetIfUnsignedGreaterEqual(code []byte, destination cpu.Register) []byte {
-	return append(code, 0x0F, 0x93, ModRM(AddressDirect, 0, byte(destination)))
+	return encode(code, AddressDirect, 0, destination, 1, 0x0F93)
 }
 
 // SetIfUnsignedLess sets the register to 1 if the result was less using unsigned comparison, otherwise to 0.
 func SetIfUnsignedLess(code []byte, destination cpu.Register) []byte {
-	return append(code, 0x0F, 0x92, ModRM(AddressDirect, 0, byte(destination)))
+	return encode(code, AddressDirect, 0, destination, 1, 0x0F92)
 }
 
 // SetIfUnsignedLessEqual sets the register to 1 if the result was less or equal using unsigned comparison, otherwise to 0.
 func SetIfUnsignedLessEqual(code []byte, destination cpu.Register) []byte {
-	return append(code, 0x0F, 0x96, ModRM(AddressDirect, 0, byte(destination)))
+	return encode(code, AddressDirect, 0, destination, 1, 0x0F96)
 }
