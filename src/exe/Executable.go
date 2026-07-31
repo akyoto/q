32e63cf679c15a593ce2e2409589121a0f233f57
Eduard Urbach <eduard@urbach.dev>
2026-07-31 15:10:01 +0200

    Refactored section offset calculation

--- src/exe/Executable.go
+++ src/exe/Executable.go
@@ -23,44 +23,45 @@ func New(headerEnd int, fileAlign int, memoryAlign int, congruent bool, embedHea
 
 // AddSections adds the given byte slices as sections to the executable.
 func (exe *Executable) AddSections(raw ...[]byte) {
-	if exe.Sections == nil {
-		exe.Sections = make([]*Section, len(raw))
-	}
-
-	for i, data := range raw {
+	for _, data := range raw {
 		if len(data) == 0 {
 			data = []byte{0}
 		}
 
-		exe.Sections[i] = &Section{Bytes: data}
+		index := len(exe.Sections)
+		section := &Section{Bytes: data}
+		exe.update(section, index)
+		exe.Sections = append(exe.Sections, section)
 	}
-
-	exe.update()
 }
 
-// update recalculates all section offsets.
-func (exe *Executable) update() {
-	first := exe.Sections[0]
+// update recalculates the section offsets.
+func (exe *Executable) update(section *Section, index int) {
+	if index == 0 {
+		exe.updateFirst(section)
+		return
+	}
 
+	previous := exe.Sections[index-1]
+	section.FileOffset, section.Padding = AlignPad(previous.FileOffset+len(previous.Bytes), exe.fileAlign)
+	section.MemoryOffset = Align(previous.MemoryOffset+len(previous.Bytes), exe.memoryAlign)
+
+	if exe.congruent && exe.fileAlign != exe.memoryAlign {
+		section.MemoryOffset += section.FileOffset % exe.memoryAlign
+	}
+}
+
+// updateFirst recalculates the section offsets for the first section.
+func (exe *Executable) updateFirst(section *Section) {
 	if exe.embedHeaders {
-		first.FileOffset, first.Padding = AlignPad(exe.headerEnd, 0x40)
-		first.MemoryOffset = first.FileOffset
+		section.FileOffset, section.Padding = AlignPad(exe.headerEnd, 0x40)
+		section.MemoryOffset = section.FileOffset
 	} else {
-		first.FileOffset, first.Padding = AlignPad(exe.headerEnd, exe.fileAlign)
-		first.MemoryOffset = Align(exe.headerEnd, exe.memoryAlign)
+		section.FileOffset, section.Padding = AlignPad(exe.headerEnd, exe.fileAlign)
+		section.MemoryOffset = Align(exe.headerEnd, exe.memoryAlign)
 	}
 
 	if exe.congruent && exe.fileAlign != exe.memoryAlign {
-		first.MemoryOffset += first.FileOffset % exe.memoryAlign
-	}
-
-	for i, section := range exe.Sections[1:] {
-		previous := exe.Sections[i]
-		section.FileOffset, section.Padding = AlignPad(previous.FileOffset+len(previous.Bytes), exe.fileAlign)
-		section.MemoryOffset = Align(previous.MemoryOffset+len(previous.Bytes), exe.memoryAlign)
-
-		if exe.congruent && exe.fileAlign != exe.memoryAlign {
-			section.MemoryOffset += section.FileOffset % exe.memoryAlign
-		}
+		section.MemoryOffset += section.FileOffset % exe.memoryAlign
 	}
 }
